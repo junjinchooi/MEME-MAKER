0f3d9e1da455a8173519a3af279eb09967255bd7
junjin <wnswls5264@gmail.com>
2024-05-21 17:05:50 +0900

    20240521_fill함수 설명

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
-ctx.rect(50, 50, 100, 100)
-ctx.rect(150, 150, 100, 100)
-ctx.rect(250, 250, 100, 100)
-ctx.fill();
 
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100)
-ctx.rect(450, 450, 100, 100)
-ctx.fillStyle="red";
-setTimeout(() => {
-    ctx.fill();
-},5000)
-ctx.fill();
+// fillRect() -> fill() + rect()
+// rect() -> moveTo() + lineTo()
+
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.fill();         // 내부 색을 채움
